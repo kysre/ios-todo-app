c6dedc1d1baf35fc283f18b0c68e435e5ca1e2d8
kasra <kasrahajian2002@gmail.com>
2022-06-09 01:49:09 +0430

    Initial Commit

---
+++ ios-todo-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-todo-app
+//
+//  Created by Kasra Hajian on 6/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-todo-app/ios_todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_todo_appApp.swift
+//  ios-todo-app
+//
+//  Created by Kasra Hajian on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
